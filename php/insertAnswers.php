b6c56798d85ba2af941acc6b9a94b7b1c3480f9a
dede-amdp <73134218+dede-amdp@users.noreply.github.com>
2021-06-03 17:32:41 +0200

    Corretta data compilazione questionario

--- php/insertAnswers.php
+++ php/insertAnswers.php
@@ -7,7 +7,7 @@
         $conn = open_conn();
         $matricola = mysqli_real_escape_string($conn, $_SESSION['matricola']);
         $id = explode(',', mysqli_real_escape_string($conn, $_POST['id']));
-        $table = 'questionario (risposte, id_esame_corso, id_esame_attdid, ord_esame_attdid, id_esame_docente)';
+        $table = 'questionario (risposte, data_compilazione, id_esame_corso, id_esame_attdid, ord_esame_attdid, id_esame_docente)';
         $risposte = '';
         foreach($_POST as $key => $value){
             if($key != 'id'){
@@ -15,7 +15,7 @@
             }
         }
         if(verify($conn, $id)){ // verifica che i dati siano coerenti
-            if(insert_DB($conn, $table, $risposte, ...$id)){
+            if(insert_DB($conn, $table, $risposte, date('Y-m-d'), ...$id)){
                 $query = 'UPDATE frequentato SET questionario=1 WHERE matricola_studente= ? AND id_corso_esame = ? AND id_attdid_esame = ? AND ord_attdid_esame = ? AND id_docente_esame = ?;';
                 if(update_DB($conn, $query, $matricola, ...$id)) $_SESSION['error_msg'] = 'Questionario compilato';
                 // seguono messaggi di errore
